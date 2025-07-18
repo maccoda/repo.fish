93a9295289bc19607e1ae78afbdc4a9f72fbb304
Dylan Maccora <the.maccoda@gmail.com>
2025-07-18 10:32:06 +1000

    fix: Handle merge conflicts for main
    When running repo main and there are conflicts the stash doesn't
    complete correctly. This should now error

--- functions/_repo_main.fish
+++ functions/_repo_main.fish
@@ -1,11 +1,24 @@
-
 function _repo_main
     set repo_status (git status --porcelain)
     if test -n "$repo_status"
         echo "Detected local changes, stashing all"
         git stash --include-untracked >/dev/null
     end
-    set main_branch (_git_main_branch)
+
+    set repo_status (git status --porcelain)
+    if test -n "$repo_status"
+        echo "ERROR: Seems to have merge conflicts, resolve first then try again"
+        git status --short
+        return 1
+    end
+
+    git switch main &>/dev/null; or git switch master &>/dev/null
+    if test $status -ne 0
+        echo "ERROR: Unknown main branch"
+        git branch
+        return 1
+    end
+    set main_branch (git branch --show-current)
     echo "Determined primary branch is $main_branch"
 
     _repo_spin --title "Pulling latest changes on $main_branch" -- git pull --prune --tags origin $main_branch
