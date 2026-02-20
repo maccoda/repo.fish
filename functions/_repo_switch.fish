a925cd6223760b2436a1583200c726ce7b999026
Dylan Maccora <the.maccoda@gmail.com>
2026-02-20 16:11:59 +1100

    feat(switch): Add ability to prune branches

--- functions/_repo_switch.fish
+++ functions/_repo_switch.fish
@@ -7,7 +7,10 @@ function _repo_switch
     set git_branch_cmd "git branch --all --format='%(refname:short)'"
     set switch_log_cmd "git log --oneline --no-decorate --color=always"
     # TODO: It would be nice if you could change the filter to only include locals, then all
-    set selection (eval $git_branch_cmd | fzf --height "~20" --preview-window="75%" --bind "ctrl-r:reload(git fetch --all --prune &> /dev/null && $git_branch_cmd)" --header "C-r to refresh" --preview "$switch_log_cmd {}")
+    set selection (eval $git_branch_cmd | fzf --height "~20" --preview-window="70%"\
+    --bind "ctrl-r:reload(git fetch --all --prune &> /dev/null && $git_branch_cmd),ctrl-x:reload(_repo_prune_branches --force &> /dev/null; $git_branch_cmd)" \
+    --header "C-r to refresh, C-x to prune" \
+    --preview "$switch_log_cmd {}")
     if test -z $selection
         return
     end
